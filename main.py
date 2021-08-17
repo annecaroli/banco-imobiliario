7630bf20e564f0a0ed45c1cc61627a94e7b50568
annecaroli <carolinesilva4@hotmail.com>
2021-08-17 17:13:28 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Desafio Keeggo
+Anne Caroline Silva
+2021-08-17
+
+"""
+
+
+def main():
+    # jogo
+
+
+if __name__ == '__main__':
+    main()
---
+++ property.py
@@ -0,0 +1,6 @@
+class Property:
+    def __init__(self, name, c_sale, c_rent, has_owner):
+        self.name = name
+        self.c_sale = c_sale
+        self.s_rent = c_rent
+        self.has_owner = has_owner
